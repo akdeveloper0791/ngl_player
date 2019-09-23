2cda5c2b91544eb065a1b15d7ff70c70b1a2fab4
pavanibuchupalli1993 <b.pavanireddy222@gmil.com>
2019-09-23 14:19:41 +0530

    Bug Fixes

--- app/src/main/java/com/ibetter/www/adskitedigi/adskitedigi/settings/signage_manager_settings/enter_prise_mode/EnterPriceSettings.java
+++ app/src/main/java/com/ibetter/www/adskitedigi/adskitedigi/settings/signage_manager_settings/enter_prise_mode/EnterPriceSettings.java
@@ -36,7 +36,6 @@ public class EnterPriceSettings extends Fragment implements CompoundButton.OnChe
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_WIFI_STATE,
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.FOREGROUND_SERVICE,
             Manifest.permission.CAMERA};
 
     @Override
